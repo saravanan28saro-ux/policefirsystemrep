6506fefb606bd865c1f217d4085585b7d6d05042
saravanan28saro-ux <saravanan28saro@gmail.com>
2026-02-15 18:54:54 +0530

    files modifide

--- policefirsystem/src/componunts/Contact.js
+++ policefirsystem/src/componunts/Contact.js
@@ -3,7 +3,7 @@ const Contact=()=>{
     return <ContactContainer>
         <h1>CONTACT US</h1>
         <p>Saravanan -7695969534 saravanan28saro@gmail.com</p>
-        <p>Harish Vijay -8995953534 harish19vijay@gmail.com</p>
+        <p>Harish Vijay -9884831290 harish19vijay@gmail.com</p>
     </ContactContainer>
 }
 export default Contact;
